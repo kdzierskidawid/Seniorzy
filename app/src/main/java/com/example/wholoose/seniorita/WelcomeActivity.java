81202d7ebe57b865a7153c06c69c2fbd140a7c2e
Marek <marek.grzejszczak@wp.pl>
2019-05-11 01:41:16 +0200

    - Dodane ekrany: -powitania -logowanie -rejestracja + datepicker

---
+++ app/src/main/java/com/example/wholoose/seniorita/WelcomeActivity.java
@@ -0,0 +1,23 @@
+package com.example.wholoose.seniorita;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+    public void LoginClick(View v){
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+    public void RegisterClick(View v){
+        Intent intent = new Intent(this,RegisterActivity.class);
+        startActivity(intent);
+    }
+}
