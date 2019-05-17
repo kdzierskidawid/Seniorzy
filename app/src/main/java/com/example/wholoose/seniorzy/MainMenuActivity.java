15eac646e3d6ea5731acef3e75426ad9aebaa744
Dawid Kedzierski <dawid1kedzierski@gmail.com>
2019-05-17 17:21:40 +0200

    Rejestracja + logowanie
    Rejestracja + logowanie

---
+++ app/src/main/java/com/example/wholoose/seniorzy/MainMenuActivity.java
@@ -0,0 +1,56 @@
+package com.example.wholoose.seniorzy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    Button btn_logout;
+
+    private FirebaseAuth firebaseAuth;
+    TextView welcome_txt;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        setup();
+        firebaseAuth = FirebaseAuth.getInstance();
+        //if the user is not logged in
+        //that means current user will return null
+        if(firebaseAuth.getCurrentUser() == null){
+            //closing this activity
+            finish();
+            //starting login activity
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        welcome_txt.setText("Witaj, " + user.getEmail());
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+    public void logout(){
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void setup(){
+        btn_logout = (Button) findViewById(R.id.btn_logout);
+        welcome_txt = (TextView) findViewById(R.id.welcome_txt);
+    }
+}
